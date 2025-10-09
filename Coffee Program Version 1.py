b3d93c4655545d8c2b3b91e2cb8d8f9c183e934e
Snowy <joanna.liao@kingsway.school.nz>
2025-10-10 11:02:10 +1300

    Coffee Program Ver 2
    Gave a response 
    While loop

--- Coffee Program Version 1.py
+++ Coffee Program Version 1.py
@@ -3,17 +3,34 @@
 #Date: 01-10-2025
 
 
-#Version 1.0
+#Version 2.0
 #TODO: Ask the user if they like coffee
 #      Record the anser
 #      Give a response back to user
 
+#Version 1
 #Ask the user whether they like coffee or not
-Like_coffee=input("Do you like to drink coffee? ")
+'''Like_coffee=input("Do you like to drink coffee? ")
 print(f'Your answer was "{Like_coffee}".')
 
 if Like_coffee=="yes":
  print("That is great, I like coffee too")
 
 else: 
-    print("You are missing out! Why not give it a try?")
+    print("You are missing out! Why not give it a try?")'''
+
+#Version 2.0
+#While loop
+keep_going = ""
+while keep_going =="":
+    like_coffee=input("Do you like to drink coffee? ")
+ 
+    if like_coffee=="yes" or like_coffee== "Yes" or like_coffee== "y":
+        print("That is great, I like coffee too")
+
+    elif like_coffee== "no" or like_coffee== "No" or like_coffee== "n": 
+        print("You are missing out! Why not give it a try?")
+
+    else:
+        print("I don't understand")
+ 
