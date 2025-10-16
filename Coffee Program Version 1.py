d17476ad133695f7128062b6ddb56a7c8bd454af
Snowy <joanna.liao@kingsway.school.nz>
2025-10-17 10:45:36 +1300

    Program Version 3

--- Coffee Program Version 1.py
+++ Coffee Program Version 1.py
@@ -21,16 +21,44 @@ else:
 
 #Version 2.0
 #While loop
-keep_going = ""
+'''keep_going = "" #The variable contains an empty string
 while keep_going =="":
     like_coffee=input("Do you like to drink coffee? ")
  
     if like_coffee=="yes" or like_coffee== "Yes" or like_coffee== "y":
         print("That is great, I like coffee too")
-
+        keep_going= "finished"
     elif like_coffee== "no" or like_coffee== "No" or like_coffee== "n": 
         print("You are missing out! Why not give it a try?")
-
+        keep_going="finished" 
     else:
-        print("I don't understand")
- 
+        print("I don't understand")'''
+        
+
+
+ #Version 3.0
+ #Making program more pythonic
+keep_going=""
+while keep_going== "":
+    #convert answer to lower cause using .lower()
+    like_coffee= input("Do you like coffee? \n").lower()
+    if like_coffee== "yes" or like_coffee== "y":
+        print("That's great! I like coffee too")
+        
+    elif like_coffee== "no" or like_coffee== "n":
+        print("You are missing out! Why not give it a try?")
+
+        like_tea= input("Do you like tea instead?\n").upper()
+        if like_tea == "YES" or like_tea == "Y":
+         print("Good for you, you should also give coffee a try")
+    
+        elif like_tea == "NO" or like_tea == "N":
+            print("I am sorry. That is all I have for you")
+
+        else:
+         print("I don't understand. Please answer with either yes or no")
+        
+    else: #error message
+         print("I don't understand")
+
+    keep_going = input ("Press <return> to continue, or any other kep to quit. Thanks!")
